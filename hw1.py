b1a04ed0c5660ad93f057f15f61563bf1b57e699
prakarshpandey <ppandey@g.hmc.edu>
2018-01-23 20:20:54 -0800

    finish hw 1

--- hw1.py
+++ hw1.py
@@ -1,19 +1,69 @@
 """
+Prakarsh Pandeb, Harveb Mudd College
 Homework 1:
 Problem Statement:
 Design a program that calculates and outputs the least common multiple of two
-integers a and b, a, b >= 3. The user should as
-
+integers a and b, a, b >= 3. The program would ask the user to enter a and b
+from the keyboard, i.e., the solution needs to be of a general nature.
+In this HW, I want you to present 2 versions of the solution: first, do not use
+the logical operator AND. Second, do not use the logical operator OR
 """
+import sys
+# Solution 2. Does not use the logical AND operator
+def lcm_no_or(a, b):
+    b_init = b
+    a_init = a
+    if b > a:
+        return lcm_no_or(b, a)
+    elif b == a:
+        return a
+    else:
+        # set initial value for lcm
+        lcm = b
+        while True:
+            # keep adding b_init to lcm and b until they are equal to a
+            lcm += b_init
+            b += b_init
+            # in this case we know what the lcm condition is
+            if lcm == a and lcm == b:
+                return a
+            elif b > a:
+                a += a_init
 
-def lcm(x, y):
-    y_init = y
-    x_init = x
-    if y > x:
-        return lcm(y, x)
-    while True:
-        y += y_init
-        if x == y:
-            return x
-        elif y > x:
-            x += x_init
+# Solution 1. Does not use the logical OR operator
+def lcm_no_and(a, b):
+    b_init = b
+    a_init = a
+    if b > a:
+        return lcm_no_or(b, a)
+    elif b == a:
+        return a
+    else:
+        # set initial value for lcm
+        lcm = b
+        while True:
+            # keep adding b_init to lcm and b until they are equal to a
+            lcm += b_init
+            b += b_init
+            # apply DeMorgan's Law to remove the logical AND
+            # in this case we know what the lcm condition is
+            if not(not lcm == a or not lcm == b):
+                return a
+            elif b > a:
+                a += a_init
+def main():
+    print("Please enter two integers a and b")
+    try:
+        a = int(input())
+        b = int(input())
+    except ValueError:
+        print("You did not enter an int. Exiting")
+        sys.exit()
+    if a < 3 or b < 3:
+        print("a and b have to be greater than 3. Exiting")
+        sys.exit()
+    else:
+        print("The least common multiple of", a, "and", b, "is", lcm_no_or(a, b))
+
+if __name__ == "__main__":
+    main()
